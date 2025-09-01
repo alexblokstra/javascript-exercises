31e7b20b0b169eed245b57825f122133bb761b15
alexblokstra <alexblokstra@live.nl>
2025-09-01 14:46:16 +0200

    simplify code

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,9 +1,6 @@
 const reverseString = function(str) {
- var splitString = str.split("");
- var reverseString = splitString.reverse();
- var result = reverseString.join("");
+ return str.split("").reverse().join("");
 
- return result;
 };
 
 // Do not edit below this line
