485f61e50c4898531d636ca065dfacf0426a29d3
alexblokstra <alexblokstra@live.nl>
2025-09-01 14:43:39 +0200

    Complete 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+ var splitString = str.split("");
+ var reverseString = splitString.reverse();
+ var result = reverseString.join("");
 
+ return result;
 };
 
 // Do not edit below this line
