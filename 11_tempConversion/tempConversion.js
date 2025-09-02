9ffbcbd6e294905b6f795ac190350dbda2f32178
alexblokstra <alexblokstra@live.nl>
2025-09-02 17:11:57 +0200

    complete 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(a) {
+  const celsius = (a - 32) * 5 / 9;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(a) {
+  const fahrenheit = (a * 9 / 5) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
